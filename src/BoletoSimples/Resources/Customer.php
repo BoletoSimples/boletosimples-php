0ccc8565a32138433b5dc2e8751142c1d7cb8695
Kivanio Barbosa <kivanio@gmail.com>
2016-12-26 15:58:08 -0300

    add search by cnpj_cpf and email at Customer

--- src/BoletoSimples/Resources/Customer.php
+++ src/BoletoSimples/Resources/Customer.php
@@ -3,4 +3,18 @@
 namespace BoletoSimples;
 
 class Customer extends BaseResource {
+  public static function cnpj_cpf($cnpj_cpf) {
+    if (!$cnpj_cpf) {
+      throw new \Exception("Couldn't find ".get_called_class()." without an cnpj or cpf.");
+    }
+    $response = self::sendRequest('GET', 'customers/cnpj_cpf', ['query' => ['q' => $cnpj_cpf]]);
+    return $this->parseResponse($response);
+  }
+  public static function email($email) {
+    if (!$email) {
+      throw new \Exception("Couldn't find ".get_called_class()." without an email.");
+    }
+    $response = self::sendRequest('GET', 'customers/email', ['query' => ['q' => $email]]);
+    return $this->parseResponse($response);
+  }
 }
