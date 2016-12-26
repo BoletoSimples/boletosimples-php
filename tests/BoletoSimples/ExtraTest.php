17f649ce7a06cce1f3d4f615155e9adbba30a3dd
Kivanio Barbosa <kivanio@gmail.com>
2016-12-26 16:47:17 -0300

    fix ExtraTest.php

--- tests/BoletoSimples/ExtraTest.php
+++ tests/BoletoSimples/ExtraTest.php
@@ -16,6 +16,6 @@ class ExtraTest extends PHPUnit_Framework_TestCase {
    */
   public function testUserinfo() {
     $this->subject = BoletoSimples\Extra::userinfo();
-    $this->assertEquals(array_keys($this->subject), ["id","login_url","email","account_type","first_name","middle_name","last_name","full_name","cpf","date_of_birth","mother_name","father_name","account_level","phone_number","address_street_name","address_number","address_complement","address_neighborhood","address_postal_code","address_city_name","address_state","business_name","business_cnpj","business_legal_name"]);
+    $this->assertEquals(array_keys($this->subject), ["id","login_url","email","account_type","first_name","middle_name","last_name","full_name","cpf","date_of_birth","mother_name","father_name","account_level","phone_number","address_street_name","address_number","address_complement","address_neighborhood","address_postal_code","address_city_name","address_state","business_name","business_cnpj","business_legal_name","balance_cents"]);
   }
 }
