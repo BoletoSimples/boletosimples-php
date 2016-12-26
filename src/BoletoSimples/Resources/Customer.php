8188fa314806e72b40fc0ee887bcd4f09040be5c
Kivanio Barbosa <kivanio@gmail.com>
2016-12-26 16:19:20 -0300

    return Customer object

--- src/BoletoSimples/Resources/Customer.php
+++ src/BoletoSimples/Resources/Customer.php
@@ -8,13 +8,13 @@ class Customer extends BaseResource {
       throw new \Exception("Couldn't find ".get_called_class()." without an cnpj or cpf.");
     }
     $response = self::sendRequest('GET', 'customers/cnpj_cpf', ['query' => ['q' => $cnpj_cpf]]);
-    return $this->parseResponse($response);
+    return new Customer($response->json());
   }
   public static function email($email) {
     if (!$email) {
       throw new \Exception("Couldn't find ".get_called_class()." without an email.");
     }
     $response = self::sendRequest('GET', 'customers/email', ['query' => ['q' => $email]]);
-    return $this->parseResponse($response);
+    return new Customer($response->json());
   }
 }
