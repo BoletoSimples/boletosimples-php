e2d4eed81af9ce4da2b7f6591fc441e10523ae2f
Kivanio Barbosa <kivanio@gmail.com>
2016-12-26 17:17:55 -0300

    fix customer tests

--- tests/BoletoSimples/Resources/CustomerTest.php
+++ tests/BoletoSimples/Resources/CustomerTest.php
@@ -40,7 +40,7 @@ class CustomerTest extends PHPUnit_Framework_TestCase {
 
     $this->assertTrue($customer instanceof \BoletoSimples\Customer);
     $this->assertFalse ($customer->isPersisted());
-    $this->assertEquals ($customer->response_errors, ["person_name"=>["não pode ficar em branco"],"cnpj_cpf"=>["não pode ficar em branco"],"zipcode"=>["não pode ficar em branco"]]);
+    $this->assertEquals ($customer->response_errors, ["person_name"=>["não pode ficar em branco"],"cnpj_cpf"=>["não pode ficar em branco"],"zipcode"=>["não pode ficar em branco"],"state"=>["não pode ficar em branco"],"address"=>["não pode ficar em branco"],"neighborhood"=>["não pode ficar em branco"],"city_id"=>["não pode ficar em branco"]]);
   }
 
   /**
@@ -49,7 +49,7 @@ class CustomerTest extends PHPUnit_Framework_TestCase {
   public function testCreateSuccess() {
     $customer = BoletoSimples\Customer::create(array (
       'person_name' => "Joao da Silva",
-      'cnpj_cpf' => "321.315.217-07",
+      'cnpj_cpf' => "732.327.384-60",
       'email' => "cliente@example.com",
       'address' => "Rua quinhentos",
       'city_name' => "Rio de Janeiro",
@@ -65,7 +65,7 @@ class CustomerTest extends PHPUnit_Framework_TestCase {
     $this->assertTrue($customer instanceof \BoletoSimples\Customer);
     $this->assertTrue ($customer->isPersisted());
     $this->assertEquals ($customer->response_errors, array());
-    $this->assertEquals (array_keys($customer->attributes()), ["id","city_name","person_name","address","address_complement","address_number","mobile_number","cnpj_cpf","email","neighborhood","person_type","phone_number","zipcode","mobile_local_code","state","created_via_api"]);
+    $this->assertEquals (array_keys($customer->attributes()), ["id","city_name","person_name","address","address_complement","address_number","mobile_number","cnpj_cpf","email","neighborhood","person_type","phone_number","zipcode","mobile_local_code","notes","state","created_via_api","email_cc"]);
   }
 
   /**
@@ -78,7 +78,7 @@ class CustomerTest extends PHPUnit_Framework_TestCase {
 
     $this->assertTrue($customer instanceof \BoletoSimples\Customer);
     $this->assertFalse ($customer->isPersisted());
-    $this->assertEquals ($customer->response_errors, ["cnpj_cpf"=>["já está em uso"],"zipcode"=>["não pode ficar em branco"]]);
+    $this->assertEquals ($customer->response_errors, ["cnpj_cpf"=>["já está em uso"],"zipcode"=>["não pode ficar em branco"],"state"=>["não pode ficar em branco"],"address"=>["não pode ficar em branco"],"neighborhood"=>["não pode ficar em branco"],"city_id"=>["não pode ficar em branco"]]);
     $this->assertEquals (array_keys($customer->attributes()), ["person_name","cnpj_cpf"]);
   }
 
@@ -87,15 +87,22 @@ class CustomerTest extends PHPUnit_Framework_TestCase {
    */
   public function testSaveSuccess() {
     $customer = new BoletoSimples\Customer();
-    $customer->cnpj_cpf = '828.788.171-41';
+    $customer->cnpj_cpf = '036.045.511-53';
     $customer->person_name = 'Joao da Silva';
     $customer->zipcode = '12312-123';
+    $customer->email = 'cliente@example.com';
+    $customer->address = 'Rua quinhentos';
+    $customer->city_name = 'Rio de Janeiro';
+    $customer->state = 'RJ';
+    $customer->neighborhood = 'bairro';
+    $customer->address_number = '222';
+    $customer->address_complement = 'sala 5';
+    $customer->phone_number = '2112123434';
     $customer->save();
-
     $this->assertTrue($customer instanceof \BoletoSimples\Customer);
     $this->assertTrue ($customer->isPersisted());
     $this->assertEquals ($customer->response_errors, array());
-    $this->assertEquals (array_keys($customer->attributes()), ["id","city_name","person_name","address","address_complement","address_number","mobile_number","cnpj_cpf","email","neighborhood","person_type","phone_number","zipcode","mobile_local_code","state","created_via_api"]);
+    $this->assertEquals (array_keys($customer->attributes()), ["id","city_name","person_name","address","address_complement","address_number","mobile_number","cnpj_cpf","email","neighborhood","person_type","phone_number","zipcode","mobile_local_code","notes","state","created_via_api","email_cc"]);
   }
 
   /**
@@ -105,7 +112,7 @@ class CustomerTest extends PHPUnit_Framework_TestCase {
     $customer = BoletoSimples\Customer::find(self::$customer_id);
     $this->assertTrue($customer instanceof \BoletoSimples\Customer);
     $this->assertTrue ($customer->isPersisted());
-    $this->assertEquals (array_keys($customer->attributes()), ["id","city_name","person_name","address","address_complement","address_number","mobile_number","cnpj_cpf","email","neighborhood","person_type","phone_number","zipcode","mobile_local_code","state","created_via_api"]);
+    $this->assertEquals (array_keys($customer->attributes()), ["id","city_name","person_name","address","address_complement","address_number","mobile_number","cnpj_cpf","email","neighborhood","person_type","phone_number","zipcode","mobile_local_code","notes","state","created_via_api","email_cc"]);
   }
 
   /**
@@ -136,10 +143,10 @@ class CustomerTest extends PHPUnit_Framework_TestCase {
     $this->assertCount(2, $customers);
     $this->assertTrue($customers[0] instanceof \BoletoSimples\Customer);
 
-    $this->assertEquals(BoletoSimples::$last_request->total, 4);
+    $this->assertEquals(BoletoSimples::$last_request->total, 10);
     $this->assertEquals(BoletoSimples::$last_request->ratelimit_limit, 500);
-    $this->assertEquals(BoletoSimples::$last_request->ratelimit_remaining, 491);
-    $this->assertEquals(BoletoSimples::$last_request->links['last'], 'https://sandbox.boletosimples.com.br/api/v1/customers?page=2&per_page=2');
+    $this->assertEquals(BoletoSimples::$last_request->ratelimit_remaining, 485);
+    $this->assertEquals(BoletoSimples::$last_request->links['last'], 'https://sandbox.boletosimples.com.br/api/v1/customers?page=5&per_page=2');
     $this->assertEquals(BoletoSimples::$last_request->links['next'], 'https://sandbox.boletosimples.com.br/api/v1/customers?page=2&per_page=2');
   }
 
@@ -148,8 +155,9 @@ class CustomerTest extends PHPUnit_Framework_TestCase {
    */
   public function testUpdateSuccess() {
     $customer = BoletoSimples\Customer::find(self::$customer_id);
+    $customer->person_name = 'Joao da Costa';
     $this->assertTrue($customer->save());
-    $this->assertEquals($customer->person_name, 'Joao da Silva');
+    $this->assertEquals($customer->person_name, 'Joao da Costa');
   }
 
 }
