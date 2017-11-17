3bc37cbead4731010e3bc386b7ed7663c4ed3a93
Kivanio Barbosa <kivanio@gmail.com>
2017-11-17 14:33:40 -0300

    add remittance bulk

--- src/BoletoSimples/Resources/Remittance.php
+++ src/BoletoSimples/Resources/Remittance.php
@@ -3,4 +3,8 @@
 namespace BoletoSimples;
 
 class Remittance extends BaseResource {
+  public static function bulk($params = array()) {
+    $response = self::sendRequest('POST', 'remittances/bulk', ['json' => ['remittance' => $params]]);
+    return $response->json();
+  }
 }
